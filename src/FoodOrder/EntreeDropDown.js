a82933011c01052a2db26b35b2a1c7785e8cb389
Khayman King <khaymanak@gmail.com>
2022-05-03 14:34:26 -0700

    added entree dropdown menu

--- src/FoodOrder/EntreeDropDown.js
+++ src/FoodOrder/EntreeDropDown.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
-export default function EntreeDropDown() {
+export default function EntreeDropDown({ setEntreeId }) {
   return (
-    <div>EntreeDropDown</div>
+    <div>
+      <select onChange={(e) => setEntreeId(e.target.value)}>
+        <option value='1'>Lettuce</option>
+        <option value='2'>Candy Wrapper</option>
+        <option value='3'>Half Eaten Toast</option>
+      </select>
+    </div>
   );
 }
