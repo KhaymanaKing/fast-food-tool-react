28be7368adbd4ff05515ac4a05e15f974c1be679
Khayman King <khaymanak@gmail.com>
2022-05-03 14:27:49 -0700

    added state for order selection

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import './App.css';
 import { useState } from 'react';
 
 import NameInput from './Name/NameInput';
+import DrinkDropDown from './FoodOrder/DrinkDropDown';
+import EntreeDropDown from './FoodOrder/EntreeDropDown';
+import SideDropDown from './FoodOrder/SideDropdown';
 
 function App() {
   const [orderName, setOrderName] = useState('');
-  
+  const [drinkId, setDrinkId] = useState('1');
+  const [entreeId, setEntreeId] = useState('1');
+  const [sideId, setSideId] = useState('1');
   
   function handleOrderName(e){
     setOrderName(e.target.value);
