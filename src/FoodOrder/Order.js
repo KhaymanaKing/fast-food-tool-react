bb5c2e34f17c399b3cfbcfee065a02216b72ee05
Khayman King <khaymanak@gmail.com>
2022-05-03 15:19:49 -0700

    added images and slight styling

--- src/FoodOrder/Order.js
+++ src/FoodOrder/Order.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './Order.css';
 
 export default function Order({ drinkId, entreeId, sideId }) {
   return (
-    <div>
-      <img src ={`entree-${entreeId}.jpeg`}/>
-      <img src ={`drink-${drinkId}.jpeg`}/>
-      <img src ={`side-${sideId}.jpeg`}/>
+    <div className='order'>
+      <img src ={`entree-${entreeId}.png`}/>
+      <img src ={`drink-${drinkId}.png`}/>
+      <img src ={`side-${sideId}.png`}/>
     </div>
   );
 }
--- src/FoodOrder/SideDropdown.js
+++ src/FoodOrder/SideDropdown.js
@@ -4,7 +4,7 @@ export default function SideDropdown({ setSideId }) {
   return (
     <div>
       <select onChange={(e) => setSideId (e.target.value)}>
-        <option value='1'>Mud</option>
+        <option value='1'>A letter</option>
         <option value='2'>Leaves from Plant</option>
         <option value='3'>Post Card</option>
       </select>
